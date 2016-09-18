ac934f7ea9fa271a592824fc4fc551381cb008d0
arkestr18 <arkestr88@gmail.com>
2016-09-18 23:32:45 +0300

    123

--- py_weather.py
+++ py_weather.py
@@ -1,5 +1,5 @@
 import pyowm
 owm = pyowm.OWM('41b33e65cbf85aaa61dd7a5ee1e51dbd')
-observation = owm.weather_at_place('Moscow')
+observation = owm.weather_at_place('Samara')
 w = observation.get_weather()
 print(w)
